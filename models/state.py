e80e36f0ae8c912d8465b2ef988e92f7cec7ff03
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-17 19:57:34 -0400

    Improve engines state

--- models/state.py
+++ models/state.py
@@ -2,11 +2,11 @@
 """This is the state class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import Column, Integer, String
 import models
 from models.city import City
-import shlex
+from models import storage
 
 
 class State(BaseModel, Base):
@@ -18,3 +18,17 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
+
+    if models.storage_t == 'db':
+        @property
+        def cities(self):
+            """
+            Getter attribute that returns the list of City objects
+            linked to the current State
+            """
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
